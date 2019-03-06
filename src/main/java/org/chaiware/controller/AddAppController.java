b696466dad088a0feec3814daedf1d65e03e21c1
Avi Hayun <avraham2@gmail.com>
2019-03-06 17:58:25 +0200

    Add App window now closes after a save and onclick of cancel

--- src/main/java/org/chaiware/controller/AddAppController.java
+++ src/main/java/org/chaiware/controller/AddAppController.java
@@ -1,19 +1,44 @@
 package org.chaiware.controller;
 
 import javafx.beans.property.SimpleListProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.chaiware.model.App;
 
+
 public class AddAppController {
+
+  @FXML
+  private TextField tfName;
+
+  @FXML
+  private TextField tfDesc;
 
   private SimpleListProperty<App> currentApps;
 
   public void setCurrentApps(SimpleListProperty<App> currentApps) {
     this.currentApps = currentApps;
+  }
 
+  public void onSaveNewApp(ActionEvent actionEvent) {
     App app = new App();
-    app.setName("NAME");
-    app.setDesc("DESCRIPTION");
+    app.setName(tfName.getText());
+    app.setDesc(tfDesc.getText());
 
     currentApps.add(app);
+    closeWindow(actionEvent);
+  }
+
+  public void onCancel(ActionEvent actionEvent) {
+    closeWindow(actionEvent);
+  }
+
+  private void closeWindow(ActionEvent event) {
+    Node source = (Node) event.getSource();
+    Stage stage = (Stage) source.getScene().getWindow();
+    stage.close();
   }
 }
